0302585bc98f23897197a8cb7a53e0b975e42fcc
Ismaïl <nguyen.ismail@gmail.com>
2022-12-14 11:45:47 +0100

    add script which will ping the pod status according a given pod number

---
+++ index.js
@@ -0,0 +1,45 @@
+// Put your Salesforce B2C Commerce Cloud POD number in the following variable
+const pod_number = YOUR POD NUMBER HERE;
+
+
+if (https == null) {
+    var https = require('https');
+}
+  
+if (querystring == null) {
+  var querystring = require('querystring');
+}
+
+const options = {
+  hostname: 'api.status.salesforce.com',
+  port: 443,
+  path: '/v1/instances/POD' + pod_number + '/status?productKey=B2C_Commerce_Cloud&childProducts=false',
+  method: 'GET'
+}
+
+const req = https.request(options, response => {
+    if (response.statusCode == 200) {
+        var body = '';
+
+        response.on('data', function(chunk){
+            body += chunk;
+        });
+
+        response.on('end', function(){
+            var jsonBody = JSON.parse(body);
+
+            $util.insights.set('pod', jsonBody.key);
+            $util.insights.set('location', jsonBody.location);
+            $util.insights.set('status', jsonBody.status);
+            $util.insights.set('incidents', JSON.stringify(jsonBody.Incidents));
+        });
+    } else {
+        console.log('Non-200 HTTP response: ' + response.statusCode);
+    }
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
